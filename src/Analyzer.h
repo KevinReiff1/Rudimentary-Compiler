e21b1022da805dc3e6f512d602867c5822554448
Reiffk1 <AshenExile1@gmail.com>
2025-04-07 18:31:50 -0400

    Add ASTNode struct and ASTBuilder class for AST creation

--- src/Analyzer.h
+++ src/Analyzer.h
@@ -2,8 +2,24 @@
 #include <utility>
 
 #include "Parser.h"
+#include "SymbolTable.h"
 
-class SemanticAnalyzer {
+struct ASTNode {
+    NodeType type;
+    DataType data_type = DataType::Unknown;
+    std::string value; // For IDs, literals
+    int line_number;
+    std::vector<ASTNode> children;
+
+    ASTNode(const NodeType node_type, int line) : type(node_type), line_number(line) {
+    }
+
+    ASTNode(NodeType node_type, std::string val, int line = 0)
+        : type(node_type), value(std::move(val)), line_number(line) {
+    }
+};
+
+class ASTBuilder {
     CST cst;
 public:
     explicit SemanticAnalyzer(CST  cst_) :cst(std::move(cst_)) {
