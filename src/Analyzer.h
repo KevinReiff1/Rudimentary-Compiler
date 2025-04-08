5e0b134e6a6f274a8efc806a7937afe50275eb7c
Reiffk1 <AshenExile1@gmail.com>
2025-04-07 23:09:18 -0400

    Add AST node conversion methods to ASTBuilder

--- src/Analyzer.h
+++ src/Analyzer.h
@@ -24,6 +24,43 @@ struct ASTNode {
 
 class ASTBuilder {
     CST cst;
+
+    ASTNode root{NodeType::BLOCK};
+
+    std::optional<ASTNode> convert_var_declaration(const Node& node) {
+
+    }
+
+    std::optional<ASTNode> convert_node(const Node& node) {
+        switch (node.get_node_type()) {
+            case NodeType::BLOCK:
+                return convert_block(node);
+            case NodeType::VARIABLE_DECLARATION:
+                return convert_var_declaration(node);
+            case NodeType::ASSIGNMENT_STATEMENT:
+                return convert_assignment_statement(node);
+            case NodeType::IF_STATEMENT:
+                return convert_if_statement(node);
+            case NodeType::WHILE_STATEMENT:
+                return convert_while_statement(node);
+            case NodeType::PRINT_STATEMENT:
+                return convert_print_statement(node);
+            default:
+        }
+
+        return {};
+    }
+
+    std::optional<ASTNode> convert_block(const Node& node) {
+        ASTNode block{NodeType::BLOCK};
+        for (const auto &child: node.get_children()) {
+            auto child_node = convert_node(child);
+            if (child_node.has_value())
+                block.children.push_back(child_node.value());
+        }
+        return block;
+    }
+
 public:
     explicit ASTBuilder(CST cst_) : cst(std::move(cst_)) {
     }
